93c74855806928660ebf62a36655ab54f89228c7
Archisman Dawn <archismandawn7@gmail.com>
2024-07-10 16:20:48 +0530

    change

--- src/components/adminHome.js
+++ src/components/adminHome.js
@@ -25,7 +25,7 @@ export default function AdminHome({ userData }) {
   // logout
   const logOut = () => {
     window.localStorage.clear();
-    window.location.href = "./sign-in";
+    window.location.href = "https://college-server-2.onrender.com/sign-in";
   };
 
   const showPdf = (pdf) => {
